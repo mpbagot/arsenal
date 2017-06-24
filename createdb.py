9f1371ca3323a7b3af46a25d43e1917acde52e08
Mitchell Bagot <packof14eggies@gmail.com>
2017-06-25 00:28:27 +1000

    Fill in helper functions and delete functions + create database creator script

---
+++ createdb.py
@@ -0,0 +1,55 @@
+import sqlite3
+import datetime
+
+open('database.db', 'w').close()
+
+conn = sqlite3.connect('database.db')
+cur = conn.cursor()
+
+cur.execute('''
+  CREATE TABLE users (
+  	id INTEGER NOT NULL,
+  	username TEXT NOT NULL,
+  	password TEXT NOT NULL,
+  	image TEXT NOT NULL,
+    completed TEXT,
+    flagged TEXT,
+  	PRIMARY KEY (id)
+);
+'''
+)
+
+cur.execute('''
+  CREATE TABLE units (
+  	id INTEGER NOT NULL,
+  	title TEXT NOT NULL,
+  	PRIMARY KEY (id),
+);
+'''
+)
+
+cur.execute('''
+  CREATE TABLE tutorials (
+  	id INTEGER NOT NULL,
+  	unitid INTEGER NOT NULL,
+  	text TEXT,
+    image TEXT,
+    is_image BOOLEAN NOT NULL,
+  	PRIMARY KEY (id),
+  	FOREIGN KEY (unitid) REFERENCES units(id) ON DELETE CASCADE
+);
+'''
+)
+
+cur.execute('''
+  CREATE TABLE resources (
+    id INTEGER NOT NULL,
+    tutorial_id INTEGER NOT NULL,
+    link TEXT NOT NULL,
+    title TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (tutorial_id) REFERENCES tutorials(id) ON DELETE CASCADE
+);
+'''
+)
+conn.commit()
--- db.py
+++ db.py
@@ -1,25 +1,133 @@
 import sqlite3
+import datetime
+import hashlib
+
+conn = sqlite3.connect('database.db')
+conn.execute("PRAGMA foreign_keys = ON")
+conn.row_factory = sqlite3.Row
+cur = conn.cursor()
 
 class Tutorial:
     '''
-    Has a list of external resources, title, isImage(main media video or img), main image/video, uid, and paragraph list
+    Has a list of external resources, unit_id, title, isImage(main media video or img), main image/video, uid, and paragraph list
     '''
-    pass
+    def save(self):
+        pass
+
+    def add(self):
+        pass
+
+    def delete(self):
+        cur.execute('''DELETE FROM tutorials WHERE id = ?''', (self.id,))
+        conn.commit()
+
+    @staticmethod
+    def get(id):
+        pass
+
 
 class User:
     '''
     has a name, image, and completed and flagged tutorial lists.
     '''
-    pass
+    def init(self, password, shouldHash=True):
+        self.password = password
+        if shouldHash:
+            # Hash the hash 100 times for additional security!
+            for a in range(100):
+                hasher = hashlib.sha256()
+                hasher.update(self.password.encode())
+                self.password = hasher.hexdigest()
+
+    def save(self):
+        pass
+
+    def add(self):
+        pass
+
+    def delete(self):
+        cur.execute('''DELETE FROM users WHERE id = ?''', (self.id,))
+        conn.commit()
+
+    def getFlaggedTutorials(self):
+        '''
+        Get an array of Tutorial Objects corresponding to the user's flagged tutorials
+        '''
+        cur.execute('''SELECT flagged FROM users WHERE id = ?''', (self.id,))
+        row = cur.fetchone()
+        flagged = ''
+        if row:
+            flagged = row[0]
+        flagged = flagged.split('|')
+        tutorials = []
+        for tut_id in flagged:
+            t = Tutorial.get(int(tut_id))
+            if t:
+                tutorials.append(t)
+        return tutorials
+
+    def getSuggestedTutorials(self):
+        '''
+        Get an array of Tutorial Objects corresponding to the incompleted tutorials of the user
+        '''
+        # Get the completed tutorial ids
+        cur.execute('''SELECT completed FROM users WHERE id = ?''', (self.id,))
+        row = cur.fetchone()
+        completed = ''
+        if row:
+            completed = row[0]
+        completed = completed.split('|')
+        # Generate the condition statement for the SQL
+        condition = " && ".join(["id != {}".format(int(i)) for i in completed])
+        # Pull the tutorials from the database
+        cur.execute('''SELECT * FROM tutorials WHERE {}'''.format(condition))
+        rows = cur.fetchall()
+        tutorials = []
+        for row in rows:
+            uid, unitId, text, image, isImage = row
+            # If there are fewer than 5 tutorials currently in the list
+            # then append the tutorial
+            if len(tutorials) < 5:
+                tutorials.append(Tutorial.get(uid))
+        # Return the tutorial list.
+        return tutorials
+
+    @staticmethod
+    def get(id):
+        pass
 
 class Unit:
     '''
-    has a title, list of tutorials, and uid
+    Has a title and uid
     '''
-    pass
+    def save(self):
+        pass
+
+    def add(self):
+        pass
+
+    def delete(self):
+        cur.execute('''DELETE FROM units WHERE id = ?''', (self.id,))
+        conn.commit()
+
+    @staticmethod
+    def get(id):
+        pass
 
 class Resource:
     '''
-    has a title, and a link
+    has a title, tutorial_id, and a link
     '''
-    pass
+    def save(self):
+        pass
+
+    def add(self):
+        pass
+
+    def delete(self):
+        cur.execute('''DELETE FROM resources WHERE id = ?''', (self.id,))
+        conn.commit()
+
+    @staticmethod
+    def get(id):
+        pass
