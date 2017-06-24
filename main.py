8d3d4864ecfa301c8899f309d06e02ea271d466a
Mitchell Bagot <packof14eggies@gmail.com>
2017-06-24 13:12:04 +1000

    Update JQuery and add dragndrop uploader for SVG Checker

--- main.py
+++ main.py
@@ -6,40 +6,72 @@ def index_handler(request):
     '''
     Handle the index page
     '''
-    m = render_template('index.html', { 'title':'Index Page', 'user' : None,
+    html = render_template('index.html', { 'title':'Index Page', 'user' : None,
                                         "tutorials" : []
                                         })
-    request.write(m)
+    request.write(html)
 
 def tutorial_handler(request, tutorialId):
     '''
     Serve the Tutorial content
     '''
-    request.write('tutorial content')
+    tutorial = None
+    html = render_template('tutorial.html', { 'title':tutorial.title, 'user' : None,
+                                        "tutorial" : None
+                                        })
+    request.write(html)
 
 def tutorials_handler(request):
     '''
     Handle the tutorials page
     '''
-    request.write('tutorials')
+    html = render_template('tutorials.html', { 'title':'Tutorial Index', 'user' : None,
+                                        "units" : []
+                                        })
+    request.write(html)
 
 def flagged_handler(request):
     '''
     Handle the flagged tutorials page
     '''
-    request.write('flagged')
+    html = render_template('flagged.html', { 'title':'Flagged Tutorials', 'user' : None})
+    request.write(html)
 
 def checker_handler(request):
     '''
     Handle the SVG Checker page
     '''
-    request.write('SVG Checker')
+    html = render_template('checker.html', { 'title':'SVG Checker', 'user' : None})
+    request.write(html)
+
+def upload_handler(request):
+    '''
+    Handle the AJAX post request
+    '''
+    method = request.request.method
+    if method != "POST":
+        checker_handler(request)
+    elif method == "POST":
+        fileTuple = request.get_file('laser_img')
+        fileData = fileTuple[-1]
+        fileName = fileTuple[0]
+        result = processSVG(fileData)
+        response = 'File valid!' if result[0] else 'Invalid File! '+result[1]
+        request.write(fileName+'|'+response)
 
 def http404_handler(request):
     '''
     Handle a 404 page request
     '''
-    request.write('You suck.')
+    html = render_template('404.html', {'title':'Error 404', 'user' : None})
+    request.write(html)
+
+def processSVG(data):
+    '''
+    Parse the SVG data and determine if it is
+    a valid laser cutting schematic or not
+    '''
+    return (True, '')
 
 # Initialise the server
 server = Server(hostname='0.0.0.0', port=80)
@@ -49,6 +81,7 @@ server.register(r'/tutorials', tutorials_handler)
 server.register(r'/flagged', flagged_handler)
 server.register(r'/svgchecker', checker_handler)
 server.register(r'/tutorial/([0-9]+)', tutorial_handler)
+server.register(r'/upload', upload_handler)
 server.register(r'/.+', http404_handler)
 
 # Run the server
---
+++ static/js/checker.js
@@ -0,0 +1,72 @@
+var isAdvancedUpload = function() {
+  var div = document.createElement('div');
+  return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+}
+
+var $form = $('.box');
+
+if (isAdvancedUpload()) {
+  $form.addClass('has-advanced-upload');
+
+  var droppedFiles = false;
+
+  $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $form.removeClass('is-error');
+    $form.removeClass('is-success');
+  })
+  .on('dragover dragenter', function() {
+    $form.addClass('is-dragover');
+  })
+  .on('dragleave dragend drop', function() {
+    $form.removeClass('is-dragover');
+  })
+  .on('drop', function(e) {
+    droppedFiles = e.originalEvent.dataTransfer.files;
+    $form.trigger('submit');
+  })
+  .on('submit', function(e) {
+    e.preventDefault();
+    if ($form.hasClass('is-uploading')) return false;
+
+    $form.addClass('is-uploading').removeClass('is-error');
+
+    //AJAX upload for modern browsers
+    var formData = new FormData();
+
+    for (var i = 0; i < droppedFiles.length; i++) {
+      var file = droppedFiles[i];
+      console.log(file.name.substring(file.name.length-4, file.name.length));
+      if (file.name.substring(file.name.length-4, file.name.length) != '.svg') {
+        $form.addClass('is-error');
+        $form.removeClass('is-uploading');
+        return;
+      }
+      formData.append('laser_img', file, file.name);
+    }
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('POST', '/upload', true);
+
+    xhr.onload = function() {
+      $form.removeClass('is-uploading');
+      if (xhr.status == 200) {
+        document.getElementsByClassName('box__input')[0].style.background = "url(static/img/tmp_upload/"+xhr.responseText.split('|')[0]+")";
+        document.getElementsByClassName('box__success')[0].innerHTML = xhr.responseText.split('|')[1];
+        $form.addClass('is-success');
+      } else {
+        $form.addClass('is-error');
+      }
+    };
+
+    xhr.send(formData);
+
+  })
+  .on('change', function(e) {
+    $form.trigger('submit');
+  });
+
+  document.getElementById('box__button').style.display = 'none';
+}
